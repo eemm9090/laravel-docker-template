3769fe522f9230d09c032c7cea62c78cb91d0bda
eemm9090 <51em605ek@gmail.com>
2024-09-26 17:55:11 +0900

    refactor:論理削除へ修正

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos';
     //マッピングし、クラスメソッドを用いることでDBとのやり取りができるようにしている
     //→ORM(laravelではエロクエントを持ちいている)
