32f8dce718edde745dedc61630d953066cd11841
eemm9090 <51em605ek@gmail.com>
2024-09-26 17:11:14 +0900

    improve:バリデーションルールの設定

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 use App\Todo;
 
@@ -27,7 +27,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();//個別ではなく一括で取得
 
@@ -53,14 +53,15 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
         // TODO: リクエストされた値を取得
         $inputs = $request->all();
         // TODO: 更新対象のデータを取得
         $todo = $this->todo->find($id);
         // TODO: 更新したい値の代入とUPDATE文の実行
-        $todo->fill($inputs)->save();
+        $todo->fill($inputs);
+        $todo->save();
 
         return redirect()->route('todo.show', $todo->id);
     }
